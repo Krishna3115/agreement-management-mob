2d91594654c0ce16ee03666d5b4960d1c619a50a
krishna3115 <mandiprime01@gmail.com>
2026-06-20 00:23:48 +0530

    added new Image in

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gc9uu5ak1v8"
+    "revision": "0.g6m42uabv78"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
